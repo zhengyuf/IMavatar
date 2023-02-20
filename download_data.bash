6ed66de13fa49fdd0d5941772fdd9ff1f23ff502
Yufeng Zheng <48751943+zhengyuf@users.noreply.github.com>
2023-02-20 16:27:02 +0100

    Update download_data.bash

--- download_data.bash
+++ download_data.bash
@@ -7,3 +7,10 @@ unzip yufeng.zip
 cd ../experiments/
 wget https://dataset.ait.ethz.ch/downloads/IMavatar_data/checkpoint/yufeng.zip
 unzip yufeng.zip
+## download the other subject with the following commands
+# cd ../datasets/
+# wget https://dataset.ait.ethz.ch/downloads/IMavatar_data/data/marcel.zip
+# unzip marcel.zip
+# cd ../experiments/
+# wget https://dataset.ait.ethz.ch/downloads/IMavatar_data/checkpoint/IMavatar_marcel.zip
+# unzip IMavatar_marcel.zip
